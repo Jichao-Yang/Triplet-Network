8b53e8f13937afb23a70cf2ab349f13a1e691f1a
Jichao Yang <jichaoyang@uchicago.edu>
2020-08-07 11:07:56 -0500

    Delete main_scratch.py
    Deletes scratch file

--- main_scratch.py
+++
@@ -1,6 +0,0 @@
-import toolbox as tb
-import torchvision.datasets as dsets
-train = dsets.CIFAR10("./database", download=True, train=True)
-test = dsets.CIFAR10("./database", download=True, train=False)
-a,b,c,d = tb.unpack(train,test)
-pass
